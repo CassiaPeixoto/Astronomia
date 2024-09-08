382de829542479cdfd8d28e01a471a1546d371b9
CassiaPeixoto <cassianascimento3425@outlook.com>
2024-09-08 00:26:23 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Meteoros",
+        descricao: "Meteoros são fenômenos luminosos que ocorrem quando pequenos fragmentos de rocha espacial, chamados meteoroides, entram na atmosfera terrestre. O atrito com o ar causa um aquecimento intenso, fazendo com que o meteoroide brilhe intensamente, criando o efeito visual que conhecemos como estrela cadente. Entrada na atmosfera: Ao entrar na atmosfera, o meteoroide se move em alta velocidade, causando o atrito e a incandescência. Desintegração: A maioria dos meteoroides se desintegra completamente na atmosfera, deixando um rastro luminoso.",
+        link: "https://www.ufmg.br/espacodoconhecimento/asteroides-e-meteoros/",
+        tags: "fenomenos luminosos estrela cadente"
+    },
+    {
+        titulo: "Gigantes luminosas do ceu",
+        descricao: "As estrelas são os verdadeiros motores do universo. São esferas gigantescas de gás superaquecido, principalmente hidrogênio e hélio, que emitem luz e calor graças a reações nucleares em seu núcleo.Nuvens de gás e poeira: As estrelas nascem a partir do colapso gravitacional de grandes nuvens de gás e poeira.Fusão nuclear: À medida que a nuvem se contrai, a temperatura e a pressão no centro aumentam, iniciando as reações de fusão nuclear.  Equilíbrio: A força da gravidade que puxa a matéria para dentro é equilibrada pela pressão da radiação que empurra para fora, mantendo a estrela estável.",
+        link: "https://exame.com/ciencia/nasa-publica-foto-inedita-do-nascimento-de-estrela-semelhante-ao-sol-confira/",
+        tags: "estrelas esferas fusao nuclear"
+    },
+    {
+        titulo: "Nebulosas",
+        descricao: "As nebulosas são imensas nuvens cósmicas compostas principalmente de gases (como hidrogênio e hélio) e poeira cósmica. Elas podem ter formas e tamanhos variados, desde pequenas e densas até gigantescas e difusas. As nebulosas são verdadeiros berçários de estrelas, onde novas estrelas se formam a partir da contração de nuvens de gás e poeira. Por outro lado, também podem ser os restos de estrelas que já morreram, liberando seus gases e elementos para o espaço.",
+        link: "https://science.nasa.gov/mission/hubble/science/universe-uncovered/hubble-nebulae/",
+        tags: "estrelas nuvens cosmicas"
+    }
+];
